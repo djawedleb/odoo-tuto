20f5a3752cbb1943f92844b74094738f2ee93fa7
Djaweddd <your-github-email@example.com>
2025-08-05 11:39:20 +0100

    second

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Your Name',
+    'category': 'Sales',
+    'description': """
+    Real Estate Advertisement Module
+    """,
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
